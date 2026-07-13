b097449738a864b2c655dd0769159ae45f6519a3
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-07-14 02:41:55 +0500

    updated the code and create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { use } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment, reset } from "./Redux/Features/counterSlice"
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  const count = useSelector((state)=>state.counter.value)
+  
   return (
     <div>
-      
+      <h1>{count}</h1>
+      <button onClick={()=>{
+        dispatch(increment())
+      }}>Increment</button>
+      <button onClick={()=>{
+        dispatch(decrement())
+      }}>Decrement</button>
+      <button onClick={()=>{
+        dispatch(reset())
+      }}>Reset</button>
     </div>
   )
 }
